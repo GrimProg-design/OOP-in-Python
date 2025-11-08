36363aa059f968e6e4d9d8c9e70d9b035e987e3e
GrimProg <vavilovila01@gmail.com>
2025-11-08 21:01:12 +0600

    Реализовал базовую структуру проекта

--- smart_box.py
+++ smart_box.py
@@ -4,3 +4,57 @@ class Product:
         self.price = price
         self.category = category
         self.discount = discount
+
+    def get_final_price():
+        return
+
+    def __eq__(self, value):
+        pass
+
+    def __bash__():
+        pass
+
+class ShoppingCart:
+    def __init__(self, owner, budget):
+        self.owner = owner
+        self.budget = budget
+        self.item = {}
+
+    def __len__():
+        pass
+
+    def __getitem__(key):
+        pass
+
+    def __setitem__(key, value):
+        pass
+
+    def __delitem__(key):
+        pass
+
+    def __contains__(value):
+        pass
+
+    def __iter__():
+        pass
+
+    def __add__():
+        pass
+
+    def __iadd__():
+        pass
+
+    def __str__(self):
+        pass
+
+    def get_total():
+        pass
+
+    def apply_promo_code(code):
+        pass
+
+    def can_afford():
+        pass
+
+    def most_expensive():
+        pass
