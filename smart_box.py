d3dfde2725a105168bba21e2d83dd9661602f3c6
GrimProg <vavilovila01@gmail.com>
2025-11-08 20:51:39 +0600

    first commit

---
+++ smart_box.py
@@ -0,0 +1,6 @@
+class Product:
+    def __init__(self, name, price, category, discount):
+        self.name = name
+        self.price = price
+        self.category = category
+        self.discount = discount
