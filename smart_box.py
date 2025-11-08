ef2082f331ef7b4013587e3fc5b88c63e2d9d80e
GrimProg <vavilovila01@gmail.com>
2025-11-08 21:35:02 +0600

    Добавил несколько методов

--- smart_box.py
+++ smart_box.py
@@ -5,8 +5,8 @@ class Product:
         self.category = category
         self.discount = discount
 
-    def get_final_price():
-        return
+    def get_final_price(self):
+        return self.price
 
     def __eq__(self, value):
         pass
@@ -18,16 +18,17 @@ class ShoppingCart:
     def __init__(self, owner, budget):
         self.owner = owner
         self.budget = budget
-        self.item = {}
+        self.items = {1: 'apple'}
 
-    def __len__():
-        pass
+    def __len__(self):
+        return len(self.items)
 
-    def __getitem__(key):
-        pass
+    def __getitem__(self, key):
+        return self.items[key]
 
-    def __setitem__(key, value):
-        pass
+    def __setitem__(self, key, value):
+        self.items.update({key: value})
+        return f'Товар {value} успешно добавлен'
 
     def __delitem__(key):
         pass
@@ -58,3 +59,20 @@ class ShoppingCart:
 
     def most_expensive():
         pass
+
+class User:
+    def __init__(self, name):
+        self.name = name
+        self.sum = 0
+
+    def __len__(self):
+        self.sum += self.name
+        return self.sum
+
+new_product = Product('apple', 23, 'fruit', False)
+new_product_2 = Product('banana', 27, 'fruit', False)
+box = ShoppingCart('Ilia', 200)
+print(new_product.get_final_price())
+print(len(box))
+print(box.__getitem__(1))
+print(box.__setitem__(2, 'banana'))
