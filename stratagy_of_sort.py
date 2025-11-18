b421fad233494f6262625ee9ed2088915755d1d9
GrimProg <vavilovila01@gmail.com>
2025-11-18 16:50:20 +0600

    реализовал мехонизм пузырьковой сортировки

--- stratagy_of_sort.py
+++ stratagy_of_sort.py
@@ -34,3 +34,11 @@ class DataSorter:
 
     def __call__():
         pass
+
+
+data = [12, 6, 4, 3, 8]
+for i in range(5 - 1):
+    for j in range(5 - 1 - i):
+        if data[j] > data[j+1]:
+            data[j], data[j+1] = data[j+1], data[j]
+print(data)
