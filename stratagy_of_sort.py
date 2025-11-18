2674ed95c27995b3becc2db0e2a1f22a5dca529f
GrimProg <vavilovila01@gmail.com>
2025-11-18 16:32:43 +0600

    создал каркас

---
+++ stratagy_of_sort.py
@@ -0,0 +1,36 @@
+class SortStrategy:
+    def sort(data):
+        pass
+
+class BubbleSortStrategy(SortStrategy):
+    pass
+
+class QuickSortStrategy(SortStrategy):
+    pass
+
+class MergeSortStrategy(SortStrategy):
+    pass
+
+class SmartSortStrategy(SortStrategy):
+    pass
+
+class DataSorter:
+    def __init__(self, strategy, data, statistics):
+        self.strategy = strategy
+        self.data = data
+        self.statistics = statistics
+
+    def set_strategy(strategy):
+        pass
+
+    def sort():
+        pass
+
+    def compare_strategies():
+        pass
+
+    def get_statistics():
+        pass
+
+    def __call__():
+        pass
